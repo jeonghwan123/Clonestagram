c23d5cd5017abddcb6fa9e81a00b952b15079e24
ahn_h_j <tkgkd246@gmail.com>
2025-03-17 10:30:44 +0900

    init

---
+++ src/main/java/com/goorm/clonestagram/ClonestagramApplication.java
@@ -0,0 +1,13 @@
+package com.goorm.clonestagram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClonestagramApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClonestagramApplication.class, args);
+    }
+
+}
