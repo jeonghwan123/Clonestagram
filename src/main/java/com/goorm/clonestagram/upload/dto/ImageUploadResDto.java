5cdee0fa7592b569a95226a80e70e31a6b3adfdf
ahn_h_j <tkgkd246@gmail.com>
2025-03-18 13:38:58 +0900

    feat : 사진 업로드 요청,응답 DTO 추가
    ImageUploadReqDto, ImageUploadResDto 추가

---
+++ src/main/java/com/goorm/clonestagram/upload/dto/ImageUploadResDto.java
@@ -0,0 +1,20 @@
+package com.goorm.clonestagram.upload.dto;
+
+import com.goorm.clonestagram.upload.ContentType;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+/**
+ * 이미지 업로드 응답을 위한 DTO
+ * - content, type, createdAt을 반환
+ */
+@Getter
+@Builder
+public class ImageUploadResDto {
+//    private User user;
+    private String content;
+    private ContentType type;
+    private LocalDateTime createdAt;
+}
