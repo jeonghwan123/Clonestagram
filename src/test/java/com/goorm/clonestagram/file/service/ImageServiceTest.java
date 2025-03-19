223b8543e217277c23aabb5d97824c462d6429eb
ahn_h_j <tkgkd246@gmail.com>
2025-03-19 10:32:33 +0900

    test : ImageService 테스트 추가
    파일수정테스트()
    - 파일이 올바르게 수정되는지 테스트

--- src/test/java/com/goorm/clonestagram/file/service/ImageServiceTest.java
+++ src/test/java/com/goorm/clonestagram/file/service/ImageServiceTest.java
@@ -1,6 +1,9 @@
 package com.goorm.clonestagram.file.service;
 
+import com.goorm.clonestagram.file.ContentType;
 import com.goorm.clonestagram.file.domain.Posts;
+import com.goorm.clonestagram.file.dto.ImageUpdateReqDto;
+import com.goorm.clonestagram.file.dto.ImageUpdateResDto;
 import com.goorm.clonestagram.file.dto.ImageUploadReqDto;
 import com.goorm.clonestagram.file.dto.ImageUploadResDto;
 import com.goorm.clonestagram.file.repository.PostsRepository;
@@ -15,16 +18,19 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.time.LocalDateTime;
+import java.util.Optional;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 class ImageServiceTest {
 
-    private String uploadFolder = "src/main/resources/static/uploads/";
+    private String uploadFolder = "src/main/resources/static/uploads/image";
 
     private ImageUploadReqDto imageUploadReqDto;
 
@@ -86,5 +92,34 @@ class ImageServiceTest {
         verify(postsRepository).save(any(Posts.class));
     }
 
+    @Test
+    public void 파일수정테스트(){
+        Long postSeq = 1L;
 
+        MockMultipartFile oldFile = new MockMultipartFile(
+                "file", "old-image.jpg","image/jpeg","dummy image content".getBytes()
+        );
+
+        Posts tempPost = new Posts(1L, "수정전 내용", oldFile.getName(), ContentType.IMAGE, LocalDateTime.now(),null);
+
+        MockMultipartFile newFile = new MockMultipartFile(
+                "file", "new-image.jpg","image/jpeg","dummy image content".getBytes()
+        );
+
+        ImageUpdateReqDto reqDto = new ImageUpdateReqDto();
+        reqDto.setFile(newFile);
+        reqDto.setContent("수정된 내용");
+
+        when(postsRepository.findById(eq(1L))).thenReturn(Optional.of(tempPost));
+        when(postsRepository.save(any(Posts.class))).thenAnswer(invocation -> invocation.getArgument(0));
+
+        ImageUpdateResDto imageUpdateResDto = imageService.imageUpdate(postSeq, reqDto);
+
+        assertNotNull(imageUpdateResDto);
+        assertEquals("수정된 내용", imageUpdateResDto.getContent());
+        assertNotNull(imageUpdateResDto.getUpdatedAt());
+        verify(postsRepository).findById(postSeq);
+        verify(postsRepository).save(any(Posts.class));
+
+    }
 }
