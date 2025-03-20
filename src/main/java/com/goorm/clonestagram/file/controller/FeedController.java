2fc48de5df649128ba62cad5ff00d78693c1d68b
ahn_h_j <tkgkd246@gmail.com>
2025-03-20 15:51:20 +0900

    feat : 내 피드 조회 API 컨트롤러 추가

---
+++ src/main/java/com/goorm/clonestagram/file/controller/FeedController.java
@@ -0,0 +1,34 @@
+package com.goorm.clonestagram.file.controller;
+
+import com.goorm.clonestagram.file.dto.FeedResDto;
+import com.goorm.clonestagram.file.service.FeedService;
+import com.goorm.clonestagram.util.TempUserDetail;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequiredArgsConstructor
+public class FeedController {
+
+    private final FeedService feedService;
+
+    //Todo TempUserDetail 변경
+    @GetMapping("/feeds/me")
+    public ResponseEntity<FeedResDto> myFeed(@AuthenticationPrincipal TempUserDetail userDetail,
+                                             @PageableDefault(size = 20, sort = "createdAt", direction = Sort.Direction.DESC)Pageable pageable
+
+    ){
+        Long userId = userDetail.getId();
+
+        return ResponseEntity.ok(feedService.getMyFeed(userId,pageable));
+    }
+
+
+}
