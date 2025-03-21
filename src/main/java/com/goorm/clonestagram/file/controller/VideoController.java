3cd395245709f40c7600d25621a9fd4e79929fb4
ahn_h_j <tkgkd246@gmail.com>
2025-03-21 10:06:01 +0900

    fix : 오타 수정

--- src/main/java/com/goorm/clonestagram/file/controller/VideoController.java
+++ src/main/java/com/goorm/clonestagram/file/controller/VideoController.java
@@ -14,7 +14,7 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 /**
- * 양상 업로드 요청을 처리하는 컨트롤러
+ * 영상 업로드 요청을 처리하는 컨트롤러
  * - 클라이언트로부터 영상 업로드 요청을 받아 검증 및 서비스 호출을 수행
  */
 @RestController
