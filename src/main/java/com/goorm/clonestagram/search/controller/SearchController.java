eccc1e652b2c3eb401b67c04cf9db2a75be1fd1e
ahn_h_j <tkgkd246@gmail.com>
2025-03-21 15:33:55 +0900

    feat : 검색 API 컨트롤러 추가
    searchUsers()
    - 페이지 사이즈 20, 생성순 정렬
    - GetMapping : /search/users
    - param : keyword, pageable
    - return : ResponseEntity<SearchUserResDto>
    
    searchFollowing()
    - 페이지 사이즈 20, 생성순 정렬
    - GetMapping : /search/following
    - param : userDetail, keyword, pageable
    - return : ResponseEntity<SearchUserResDto>
    
    searchFollower()
    - 페이지 사이즈 20, 생성순 정렬
    - GetMapping : /search/follower
    - param : userDetail, keyword, pageable
    - return : ResponseEntity<SearchUserResDto>
    
    searchHashTag()
    - 페이지 사이즈 20, 생성순 정렬
    - GetMapping : /search/tag
    - param : keyword, pageable
    - return : ResponseEntity<SearchResDto>

---
+++ src/main/java/com/goorm/clonestagram/search/controller/SearchController.java
@@ -0,0 +1,95 @@
+package com.goorm.clonestagram.search.controller;
+
+
+import com.goorm.clonestagram.search.dto.SearchUserResDto;
+import com.goorm.clonestagram.search.service.SearchService;
+import com.goorm.clonestagram.util.TempUserDetail;
+import jakarta.validation.constraints.NotBlank;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 검색 요청을 처리하는 컨트롤러
+ * - 클라이언트로부터 검색 키워드를 받아 검증 및 서비스 호출을 수행
+ */
+@RestController
+@RequiredArgsConstructor
+public class SearchController {
+
+    private final SearchService searchService;
+
+    /**
+     * 유저 검색
+     * - 클라이언트가 유저 이름을 검색하면 관련 데이터 반환
+     *
+     * @param keyword 클라이언트의 검색 키워드
+     * @param pageable 페이징 기능
+     * @return 유저 리스트, 검색된 데이터 수
+     */
+    @GetMapping("/search/users")
+    public ResponseEntity<SearchUserResDto> searchUsers(@RequestParam @NotBlank String keyword,
+                                                        @PageableDefault(size = 20, sort = "createdAt", direction = Sort.Direction.DESC)Pageable pageable){
+        return ResponseEntity.ok(searchService.searchUserByKeyword(keyword, pageable));
+    }
+
+    /**
+     * 팔로우 유저 검색
+     * - 클라이언트가 팔로우 중에서 유저를 찾고자 할때 사용
+     * - 클라이언트가 유저 이름을 검색하면 팔로우 중에서 관련 데이터 반환
+     *
+     * @param userDetail
+     * @param keyword 클라이언트의 검색 키워드
+     * @param pageable 페이징 기능
+     * @return 유저 리스트, 검색된 데이터 수
+     */
+    @GetMapping("/search/following")
+    public ResponseEntity<SearchUserResDto> searchFollowing(@AuthenticationPrincipal TempUserDetail userDetail,
+                                                            @RequestParam @NotBlank String keyword,
+                                                            @PageableDefault(size = 20, sort = "createdAt", direction = Sort.Direction.DESC)Pageable pageable){
+
+        Long userId = userDetail.getId();
+
+        return ResponseEntity.ok(searchService.searchFollowingByKeyword(userId,keyword,pageable));
+    }
+
+    /**
+     * 팔로워 유저 검색
+     * - 클라이언트가 팔로워 중에서 유저를 찾고자 할때 사용
+     * - 클라이언트가 유저 이름을 검색하면 팔로워 중에서 관련 데이터 반환
+     *
+     * @param userDetail
+     * @param keyword 클라이언트의 검색 키워드
+     * @param pageable 페이징 기능
+     * @return 유저 리스트, 검색된 데이터 수
+     */
+    @GetMapping("/search/follower")
+    public ResponseEntity<SearchUserResDto> searchFollower(@AuthenticationPrincipal TempUserDetail userDetail,
+                                                           @RequestParam @NotBlank String keyword,
+                                                           @PageableDefault(size = 20, sort = "createdAt", direction = Sort.Direction.DESC)Pageable pageable){
+
+        Long userId = userDetail.getId();
+
+        return ResponseEntity.ok(searchService.searchFollowerByKeyword(userId,keyword,pageable));
+    }
+
+    /**
+     *해시 태그 검색
+     * - 클라이언트가 특정 해시태그가 붙은 피드 조회시 사용
+     *
+     * @param keyword 클라이언트의 검색 키워드
+     * @param pageable 페이징 기능
+     * @return 피드 리스트, 검색된 데이터 수
+     */
+//    @GetMapping("/search/tag")
+//    public ResponseEntity<SearchResDto> searchHashTag(@RequestParam @NotBlank String keyword,
+//                                                      @PageableDefault(size = 20, sort = "createdAt", direction = Sort.Direction.DESC)Pageable pageable) {
+//        return ResponseEntity.ok(searchService.searchHashTagByKeyword(keyword,pageable));
+//    }
+}
