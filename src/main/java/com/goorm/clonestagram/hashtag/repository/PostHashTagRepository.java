949e34c3955902ce62ca4c8bf462b15096f3bfa8
ahn_h_j <tkgkd246@gmail.com>
2025-03-24 16:34:42 +0900

    feat : HashTagRepository 추가
    findPostsByHashtagKeyword : 해시태그 검색어를 통해 피드를 조회
    
    deleteAllByPostsId : 게시글 아이디와 일치하는 모든 해시태그 삭제

---
+++ src/main/java/com/goorm/clonestagram/hashtag/repository/PostHashTagRepository.java
@@ -0,0 +1,16 @@
+package com.goorm.clonestagram.hashtag.repository;
+
+import com.goorm.clonestagram.file.domain.Posts;
+import com.goorm.clonestagram.hashtag.entity.PostHashTags;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface PostHashTagRepository extends JpaRepository<PostHashTags, Long> {
+    @Query("SELECT ph.posts FROM PostHashTags ph WHERE ph.hashTags.tagContent LIKE %:keyword%")
+    Page<Posts> findPostsByHashtagKeyword(@Param("keyword") String keyword, Pageable pageable);
+
+    void deleteAllByPostsId(Long id);
+}
