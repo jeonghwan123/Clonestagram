4ad5dbb79882ed7f4b718120dc719ce0a8638dc1
ahn_h_j <tkgkd246@gmail.com>
2025-03-24 16:33:15 +0900

    feat : PostHashTags 엔티티 추가
    table : post_hashtags
    id : id
    hashtag_id : hashTags
    post_id : posts

---
+++ src/main/java/com/goorm/clonestagram/hashtag/entity/PostHashTags.java
@@ -0,0 +1,28 @@
+package com.goorm.clonestagram.hashtag.entity;
+
+import com.goorm.clonestagram.file.domain.Posts;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "post_hashtags")
+@NoArgsConstructor
+@AllArgsConstructor
+public class PostHashTags {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "hashtag_id")
+    private HashTags hashTags;
+
+    @ManyToOne
+    @JoinColumn(name = "post_id")
+    private Posts posts;
+}
