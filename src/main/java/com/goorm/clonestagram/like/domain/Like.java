748fc4d8f225f37c3a904d91f34f1f5733c36f18
ahn_h_j <tkgkd246@gmail.com>
2025-03-25 10:26:40 +0900

    fix : BaseTimeEntity 제거

--- src/main/java/com/goorm/clonestagram/like/domain/Like.java
+++ src/main/java/com/goorm/clonestagram/like/domain/Like.java
@@ -15,7 +15,7 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @AllArgsConstructor
 @Table(name = "likes")
-public class Like extends BaseTimeEntity {
+public class Like {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
